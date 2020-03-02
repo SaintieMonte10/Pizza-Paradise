2d8c4aaeb46a490a3f790af3dbdf5e714768ac8b
SaintieMonte10 <claremonte53@gmai.com>
2020-03-02 15:42:21 +0300

    added a new form

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('#checkout').click(function (event) {
+        event.preventDefault();
+        var flavour = parseInt($("#flavours :selected").val())
+        var size = parseInt($("#sizes :selected").val())
+        var crust = parseInt($("#crusts :selected").val())
+        var toppings = parseInt($("#toppings :selected").val())
+        var number = parseInt($("#number").val())
+
+        var total = flavour + size + crust + toppings;
+        var grandTotal = total * number;
+
+        $("ul#list").append(
+            "<li>" + "Your pizza flavour is : " + $("#flavours option:selected").text() + "</li>" +
+            "<li>" + "Your pizza size is : " + $("#sizes option:selected").text() + "</li>" +
+            "<li>" + "Your pizza crust is : " + $("#crusts option:selected").text() + "</li>" +
+            "<li>" + "Your pizza topping is : " + $("#toppings option:selected").text() + "</li>" +
+            "<li>" + "Number of pizza ordered is : " + number + "</li>" +
+            "<li>" + "Total amount to be paid is : Ksh" + grandTotal + "</li>");
+
+    })
+    $(".deliver").click(function () {
+        var fields = prompt("Enter your address")
+        prompt("Enter your name")
+        prompt("Enter your location")
+        alert("Your order will be delivered at" + fields)
+        alert("Delivery cost is Kshs 100")
+    })
+    $(".delivernot").click(function () {
+        alert("Pick your order in our nearest shop")
+    })
+});
